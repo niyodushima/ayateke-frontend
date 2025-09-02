30733bae5c335305d7565ac8edef58c04f7bf7bb
niyodushima <131856101+niyodushima@users.noreply.github.com>
2025-09-02 13:55:17 +0200

    Update DashboardLayout.jsx

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,18 +1,46 @@
-import { Box, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
+import { HamburgerIcon } from '@chakra-ui/icons';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 
-const DashboardLayout = () => (
-  <Flex height="100vh" overflow="hidden">
-    <Sidebar />
-    <Box flex="1" display="flex" flexDirection="column" bg="gray.50">
-      <Navbar />
-      <Box flex="1" overflowY="auto" p={6}>
-        <Outlet />
+const DashboardLayout = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Flex height="100vh" overflow="hidden">
+      {/* Sidebar for desktop */}
+      <Box display={{ base: 'none', md: 'block' }}>
+        <Sidebar />
       </Box>
-    </Box>
-  </Flex>
-);
+
+      {/* Drawer for mobile */}
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <Sidebar />
+        </DrawerContent>
+      </Drawer>
+
+      {/* Main content */}
+      <Box flex="1" display="flex" flexDirection="column" bg="gray.50">
+        <Navbar onOpenSidebar={onOpen} />
+        <Box flex="1" overflowY="auto" p={6}>
+          <Outlet />
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
 
 export default DashboardLayout;
