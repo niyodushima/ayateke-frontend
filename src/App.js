909d350118dc1e3f1653b79e5bd535babcc97395
niyodushima <131856101+niyodushima@users.noreply.github.com>
2025-09-07 11:18:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         {/* Public route */}
         <Route path="/" element={<Login />} />
-
+        <Route path="training" element={<StaffTraining />} />
         {/* Admin routes wrapped in layout */}
         <Route path="/admin" element={<DashboardLayout />}>
           <Route path="dashboard" element={<AdminDashboard />} />
